cdc62188804fb07e12031d07ac0408ce08c49751
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-07-11 17:24:17 -0300

    Requisito 04 concluido, validando form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,69 @@
+import { FormEventHandler, useState } from 'react';
+
 import Input from './Input/Input';
 import Button from './Button/Button';
+
+import { FormDataType } from './types';
 import './Form.css';
 
 type FormProps = {
   handleShowForm: (show: boolean) => void
 };
 
+const INITIAL_FORM_VALUES: FormDataType = {
+  servico: '',
+  login: '',
+  senha: '',
+  url: '',
+};
+
 function Form(props: FormProps) {
   const { handleShowForm } = props;
+
+  const [btnCadastrarEnable, setBtnCadastrarEnable] = useState(true);
+  const [formData, setFormData] = useState<FormDataType>(INITIAL_FORM_VALUES);
+  // const [formPreenchido, setFormPreenchido] = useState(false);
+
+  function validarSenha(senha: string) {
+    const regex = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,16}$/;
+    return regex.test(senha);
+  }
+
+  const validaForm = () => {
+    const validaServico = (formData.servico).trim() !== '';
+    const validaLogin = ((formData.login).trim() !== '');
+    const validaUrl = ((formData.url).trim() !== '');
+    const validaSenha = validarSenha(formData.senha);
+
+    if (validaServico && validaLogin && validaSenha && validaUrl) {
+      setBtnCadastrarEnable(false);
+    } else {
+      setBtnCadastrarEnable(true);
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+
+    validaForm();
+  };
+
   return (
-    <form className="formCadSenha">
+    <form className="formCadSenha" onSubmit={ handleSubmit }>
       <div className="formControl">
         <Input
           className="inputForm largeInput"
           id="servico"
+          value={ formData.servico }
+          onChange={ handleChange }
           label="Nome do Serviço"
         />
       </div>
@@ -21,12 +71,16 @@ function Form(props: FormProps) {
         <Input
           className="inputForm smallInput"
           id="login"
+          value={ formData.login }
+          onChange={ handleChange }
           label="Login"
         />
         <Input
           type="password"
           className="inputForm smallInput"
           id="senha"
+          value={ formData.senha }
+          onChange={ handleChange }
           label="Senha"
         />
       </div>
@@ -34,17 +88,26 @@ function Form(props: FormProps) {
         <Input
           className="inputForm largeInput"
           id="url"
+          value={ formData.url }
+          onChange={ handleChange }
           label="URL"
         />
       </div>
       <p>* campos Obrigatorios</p>
       <div className="formControl fieldButtons">
+
         <Button
           label="Cancelar"
           onClick={ () => handleShowForm(false) }
           className="btnCancelar"
         />
-        <Button label="Cadastrar" className="btnCadastrar" />
+
+        <Button
+          label="Cadastrar"
+          className="btnCadastrar"
+          onClick={ () => alert('ola pessoas') }
+          disabled={ btnCadastrarEnable }
+        />
       </div>
     </form>
   );
---
+++ src/components/types.ts
@@ -0,0 +1,6 @@
+export type FormDataType = {
+  servico: string,
+  login: string,
+  senha: string,
+  url: string,
+};
